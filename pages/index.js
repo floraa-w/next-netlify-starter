9e3addccdbe4b617b5ae38df4f8b6ca7a25cdf36
floraa-w <flora300923@gmail.com>
2025-07-20 22:01:39 -0400

    favicon

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Flora Wu</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon flower.ico" />
       </Head>
 
       <Banner />
